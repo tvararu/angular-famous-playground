49b7b66ac4c3c40893e53e244a2cba7d915d235d
Theodor Văraru <theo@vararu.org>
2014-07-06 15:58:42 +0300

    Refactor unit tests, add watch task.

--- gulp/unit-tests.js
+++ gulp/unit-tests.js
@@ -6,23 +6,35 @@ var $ = require('gulp-load-plugins')();
 
 var wiredep = require('wiredep');
 
+var bowerDeps = wiredep({
+  directory: 'app/bower_components',
+  exclude: ['bootstrap-sass-official'],
+  dependencies: true,
+  devDependencies: true
+});
+
+var testFiles = bowerDeps.js.concat([
+  'app/scripts/**/*.js',
+  'test/unit/**/*.js'
+]);
+
 gulp.task('test', function () {
-  var bowerDeps = wiredep({
-    directory: 'app/bower_components',
-    exclude: ['bootstrap-sass-official'],
-    dependencies: true,
-    devDependencies: true
-  });
-
-  var testFiles = bowerDeps.js.concat([
-    'app/scripts/**/*.js',
-    'test/unit/**/*.js'
-  ]);
-
   return gulp.src(testFiles)
     .pipe($.karma({
       configFile: 'test/karma.conf.js',
       action: 'run'
+    }))
+    .on('error', function (err) {
+      // Make sure failed tests cause gulp to exit non-zero
+      throw err;
+    });
+});
+
+gulp.task('watch:test', function () {
+  return gulp.src(testFiles)
+    .pipe($.karma({
+      configFile: 'test/karma.conf.js',
+      action: 'watch'
     }))
     .on('error', function (err) {
       // Make sure failed tests cause gulp to exit non-zero
