0097fbc9fd7cda790ac7f267c8ed431ba557f0f1
Theodor Văraru <theo@vararu.org>
2014-07-06 20:51:00 +0300

    Simple example done.

--- app/scripts/main/main-ctrl.js
+++ app/scripts/main/main-ctrl.js
@@ -2,6 +2,5 @@
 
 angular.module('angularFamousPlayground')
   .controller('MainCtrl', function ($scope, $famous) {
-    $scope.message = 'Hello, world!';
-    $scope.number = 1;
+    $scope.message = 'Hello.';
   });
