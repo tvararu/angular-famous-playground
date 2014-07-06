00d095ab29cee7b93b269158b894229cc63ca28b
Theodor Văraru <theo@vararu.org>
2014-07-06 16:46:38 +0300

    Remove other irrelevant steps from gulp serve.

--- gulp/watch.js
+++ gulp/watch.js
@@ -4,8 +4,6 @@ var gulp = require('gulp');
 
 gulp.task('watch', ['jade', 'wiredep', 'styles'], function () {
   gulp.watch('app/styles/**/*.sass', ['styles']);
-  gulp.watch('app/scripts/**/*.js', ['scripts']);
-  gulp.watch('app/images/**/*', ['images']);
   gulp.watch('bower.json', ['wiredep']);
 
   gulp.start('watch:jade');
