8481c38891b0aac74003d6161fad7ea68accb0b9
Theodor Văraru <theo@vararu.org>
2014-07-12 22:29:18 +0300

    Update homepage.

--- app/scripts/main/main-ctrl.js
+++ app/scripts/main/main-ctrl.js
@@ -1,6 +1,13 @@
 'use strict';
 
 angular.module('angularFamousPlayground')
-  .controller('MainCtrl', function ($scope, $famous) {
-    
+  .controller('MainCtrl', function ($scope, $famous, $timeout) {
+    var EventHandler = $famous['famous/core/EventHandler'];
+    $scope.evt = new EventHandler();
+
+    // Famous + Angular doesn't present a non-awkward way of solving this either.
+    $timeout(function () {
+      var h = document.querySelector('.container').scrollHeight;
+      $famous.find('.no-pointer')[0].renderNode.setSize([undefined, h]);
+    }, 100);
   });
